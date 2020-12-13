be481d8783972f82caef176eaf5e1e3a08071544
mars441998 <abdurrehmansiddiqui30@gmail.com>
2020-12-13 14:24:30 +0500

    ALU functionality extended along with code cleanup

--- ALU_microprocessor.v
+++ ALU_microprocessor.v
@@ -19,44 +19,104 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ALU_microprocessor (
-  input      [ 1:0]   alu_ctrl  ,
+  input      [ 3:0]   alu_ctrl  ,
   input      [31:0]   in_1      ,
   input      [31:0]   in_2      ,
+  input               alu_clk   ,
   output reg [31:0]   alu_rslt  ,
   output     [ 3:0]   alu_checks
 );
 
-  reg             N,Z,C,V;
+reg             N,Z,C,V;
 
-  always @(alu_ctrl,in_1,in_2)
-    case(alu_ctrl)
-      2'b00:  begin   {C,alu_rslt}=in_1+in_2;        // ADD
-        Z=(alu_rslt==32'b0);
-        N=alu_rslt[31];
-        V=in_1[31]&&in_2[31]&&(!alu_rslt[31])
-          ||(!in_1[31])&&(!in_2[31])&&alu_rslt[31];
-      end
-      2'b01:  begin   {C,alu_rslt}=in_1+(-in_2);     // SUB
-        C=!C;
-        Z=(alu_rslt==32'b0);
-        N=alu_rslt[31];
-        V=in_1[31]&&(!in_2[31])&&(!alu_rslt[31])
-          ||(!in_1[31])&&(in_2[31])&&alu_rslt[31];
-      end
-      2'b10:  begin   alu_rslt=in_1&in_2;            // AND
-        Z=(alu_rslt==32'b0);
-        N=alu_rslt[31];
-        V=0;    // Don't care
-        C=0;    // Don't care
-      end
-      2'b11:  begin   alu_rslt=in_1|in_2;            // OR (or ORR)
-        Z=(alu_rslt==32'b0);
-        N=alu_rslt[31];
-        V=0;    // Don't care
-        C=0;    // Don't care
-      end
-      default:  begin alu_rslt=0; {N,Z,C,V}=4'b0100; end
-    endcase
+always @(posedge alu_clk) begin
+  case(alu_ctrl)
+    4'b0000:  begin   
+      {C,alu_rslt} = in_1+in_2;           // ADD operation
+      Z            = (alu_rslt==32'b0);
+      N            = alu_rslt[31];
+      V            = in_1[31]&&(in_2[31])&&(!alu_rslt[31]) || (!in_1[31])&&(!in_2[31])&&alu_rslt[31];
+    end
 
-    assign alu_checks = {N,Z,C,V};  // BEWARE:  Whether these are saved in controller depends on instruction.
-endmodule
+    4'b0001:  begin   
+      {C,alu_rslt} = in_1+(-in_2);        // SUB operation
+      C            = !C;
+      Z            = (alu_rslt==32'b0);
+      N            = alu_rslt[31];
+      V            = in_1[31]&&(!in_2[31])&&(!alu_rslt[31]) || (!in_1[31])&&(in_2[31])&&alu_rslt[31];
+    end
+
+    4'b0010:  begin   
+      alu_rslt = in_1&in_2;               // AND operation
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    4'b0011:  begin  
+      alu_rslt = in_1|in_2;               // OR operation
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    4'b0100:  begin  
+      alu_rslt = ~(in_1&in_2);            // NAND operation
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    4'b0101:  begin  
+      alu_rslt = ~(in_1|in_2);            // NOR operation
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    4'b0110:  begin  
+      alu_rslt = ~(in_1^in_2);            // XNOR operation
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    4'b0111:  begin  
+      alu_rslt = in_1^in_2;               // XOR operation
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    4'b1000:  begin  
+      alu_rslt = !in_1;                   // NOT operation on first input
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    4'b1001:  begin  
+      alu_rslt = !in_2;                   // NOT operation on second input
+      Z        = (alu_rslt==32'b0);
+      N        = alu_rslt[31];
+      V        = 0;    // Don't care
+      C        = 0;    // Don't care
+    end
+
+    default:  begin                       //In default case, everything should be zero
+      alu_rslt = 0; 
+      {N,Z,C,V}= 4'b0100; 
+    end
+  endcase //case structure
+end //always block
+
+assign alu_checks = {N,Z,C,V};           // Flag for controlling values
+
+endmodule //ALU_microprocessor
